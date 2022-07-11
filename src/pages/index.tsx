59b8c8eb3432b277819ba15eaf3fb18ff4a01de5
Ismael <ismaelgomesdasilva003@gmail.com>
2022-07-11 10:59:17 -0300

    [Styles] Header responsivo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,16 @@ import HomePage from "../views/Home";
 import Feedback from "../views/Feedback";
 import Subscribe from "../views/Subscribe";
 import Footer from "../views/Footer";
+import Headeresponsive from "../components/Headeresponsive";
 
 
 
 export default function Home() {
   return (
     <>
-    
-    <Header/>
+
+      <Headeresponsive />
+      <Header/>
     <HomePage/>
     <Destination/>
     <About/>
