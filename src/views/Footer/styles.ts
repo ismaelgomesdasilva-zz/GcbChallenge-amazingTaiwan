4788c852c74ba370ded9f41e13937cadb2a36cba
Ismael <ismaelgomesdasilva003@gmail.com>
2022-07-13 16:15:08 -0300

    [responsive] footer

--- src/views/Footer/styles.ts
+++ src/views/Footer/styles.ts
@@ -1,72 +1,97 @@
 import styled from "styled-components";
+import mediaQuery from "../../utils/mediaQuery";
 export const Container = styled.div`
-display: flex;
-gap: 100px;
-width: 100%;
+  display: flex;
+  gap: 100px;
+  width: 100%;
+  padding-top: 153px;
 
-
-padding-top: 160px;
-
-
-h3{
-    font-family: 'Poppins';
-font-style: normal;
-font-weight: 700;
-font-size: 15px;
-line-height: 27px;
-
-color: #FFFFFF;
-margin-bottom: 45px;
-}
-a{
-    font-family: 'Poppins';
-font-style: normal;
-font-weight: 400;
-font-size: 13px;
-line-height: 39px;
-margin-bottom: 5px;
-color: #FFFFFF;
-cursor: pointer;
-}
-`
-export const ImagesFooter = styled.div`
-
->div{
-    margin-bottom: 116px;
-}
-`
-export const Social = styled.div`
-display: flex;
-gap: 30px;
-justify-content: center;
-img{
+  h3 {
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 27px;
+    color: #ffffff;
+    margin-bottom: 45px;
+  }
+  a {
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 39px;
+    margin-bottom: 5px;
+    color: #ffffff;
     cursor: pointer;
-}
-`
+  }
+  @media (max-width: ${mediaQuery.md}) {
+    gap: 50px;
+    padding-top: 48px;
+    a {
+      font-size: 15px;
+    }
+    h3 {
+      font-size: 20px;
+      margin: 25px 0;
+    }
+  }
+  @media (max-width: ${mediaQuery.mdl}) {
+    padding-top: 117px;
+  }
+  @media (max-width: ${mediaQuery.sml}) {
+    padding-top: 50px;
+  }
+`;
+export const ImagesFooter = styled.div`
+  > div {
+    margin-bottom: 100px;
+    width: 10rem;
+  }
+  @media (max-width: ${mediaQuery.md}) {
+    > div {
+      margin-bottom: 20px;
+    }
+  }
+`;
+export const Social = styled.div`
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+  img {
+    cursor: pointer;
+  }
+`;
 export const TableOrganization = styled.div`
-
-
-    display: flex;
-flex-direction: column;
-
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const BackgroundWaves = styled.div`
-background-image: url('/waveBottomFooter.svg');
-
-background-repeat: no-repeat;
-
-position: absolute;
-left: 0;
-right: 0;
-background-position: top;
-`
+  background-image: url("/waveBottomFooter.svg");
+  background-repeat: no-repeat;
+  background-size: 100%;
+`;
 
 export const ContainerOrganization = styled.div`
+  display: flex;
+  background-color: #293241;
+  width: 100%;
+  gap: 130px;
+  padding: 0 100px;
 
-display: flex;
-background-color: #293241;
-width: 100%;
-gap: 130px;
-padding: 0 100px;
-`
+  padding-top: 30px;
+  @media (max-width: ${mediaQuery.xl}) {
+    gap: 80px;
+  }
+  @media (max-width: ${mediaQuery.lg}) {
+    gap: 25px;
+    padding: 0;
+    
+    justify-content: center;
+  }
+  @media (max-width: ${mediaQuery.md}) {
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    justify-content: center;
+    gap: 0;
+    padding-top: 50px;
+  }
+`;
